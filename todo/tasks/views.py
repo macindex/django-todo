679f6f189ebe18425100994baa6d327cf9508487
marcio lira <machollanda@gmail.com>
2020-10-05 22:02:20 -0300

    criando layout da aplicacao

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def helloworld(request):
-	return HttpResponse('world')
-
+	return HttpResponse('Hello world!')
 
 def taskList(request):
 	return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+	return render(request, 'tasks/yourname.html', {'name': name})
 
