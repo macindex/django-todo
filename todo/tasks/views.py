41765e32cb090161359da7f68f7c0a52e6dbbb85
marcio lira <machollanda@gmail.com>
2020-10-07 15:36:19 -0300

    passando argumentos via url para template

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -6,3 +6,6 @@ def helloworld(request):
 
 def taskList(request):
 	return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+	return render(request, 'tasks/yourname.html', {'name': name})
